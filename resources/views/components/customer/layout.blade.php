33dfb979bbdcecebaab47d053f96e175f35a67fe
Zafeer <zafeer.ad999@gmail.com>
2024-09-30 03:02:15 +0530

    css fixes

--- resources/views/components/customer/layout.blade.php
+++ resources/views/components/customer/layout.blade.php
@@ -11,9 +11,9 @@
     <meta property="og:type" content="">
     <meta property="og:url" content="">
     <meta name="google" content="notranslate">
-    <meta property="og:image" content="{{ asset('customer/assets/imgs/theme/favicon.svg') }}">
+    <meta property="og:image" content="{{ config('setting_data.HEADER_LOGO') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('customer/assets/imgs/theme/favicon.svg') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ config('setting_data.HEADER_LOGO') }}">
     <link rel="stylesheet" href="{{ asset('customer/assets/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('customer/assets/css/rating.css') }}">
     {{-- <link rel="manifest" href="https://progressier.app/cTQ3i8bfuV2Pq1pyXZWw/progressier.json" /> --}}
