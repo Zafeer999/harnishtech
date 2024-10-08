ddf1dc7d1c63f8bb0a4bac391032775e41aa522c
Zafeer <zafeer.ad999@gmail.com>
2024-10-08 14:12:05 +0530

    server logo fixed

--- config/setting_data.php
+++ config/setting_data.php
@@ -5,7 +5,7 @@
   'GST_PERCENTAGE' => '20',
   'SCHEDLE_TODAY_IF_TIME_BEFORE' => '13',
   'MAX_DISCOUNT_PERCENT' => '40',
-  'HEADER_LOGO' => 'storage/logo/N15uBirlBDxM815dilBts9VXqlRZGu04zJzQ29MJ.png',
+  'HEADER_LOGO' => 'storage/logo/C6hU8FvLJajS7nj25NFEL6j49aq3N3B6Z2emTAb3.png',
   'FOOTER_ADDRESS' => 'Near TDCC bank At,post - kudus, tal - wada, dist - Palghar',
   'FOOTER_PHONE' => '(+91) 9096969667 /(+91) 7875000141',
   'FOOTER_HOURS' => '10:00 - 18:00, Mon - Sat',
